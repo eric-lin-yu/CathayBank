4c970c6d36253a912a5778509df50ad88d42066e
EricLin <zl2408399@gmail.com>
2023-08-12 12:21:01 +0800

    feat: add new color

--- CathayBank/UIColorSets.swift
+++ CathayBank/UIColorSets.swift
@@ -6,8 +6,14 @@
 import UIKit
 extension UIColor {
     //MARK: - 灰
+    /// 淡灰
+    static let lightGray: UIColor = { return UIColor.init(red: 136, green: 136, blue: 136, alpha: 0) }()
+    
     /// 木炭灰
-    static let lightGray: UIColor = { return UIColor.init(hexString: "#0x888888") }()
+    static let charcoalGray: UIColor = { return UIColor.init(red: 85, green: 85, blue: 85, alpha: 0) }()
+    
+    /// 暗灰色
+    static let dimGray: UIColor = { return UIColor.init(red: 68, green: 68, blue: 68, alpha: 0) }()
 }
 
 extension UIColor {
