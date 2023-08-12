6b526dcbcc2c3aeefd6222d97fdb32b7c78cfed4
EricLin <zl2408399@gmail.com>
2023-08-12 11:42:00 +0800

    feat: add bottom Bar

--- CathayBank/ViewController.swift
+++ CathayBank/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
     private let notificationBtn: UIButton = {
         let button = UIButton()
         let image = UIImage(named: "iconBell01Nomal")
+        button.setImage(image, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -43,19 +44,57 @@ class ViewController: UIViewController {
 
     private let bottomBarView: UIView = {
        let view = UIView()
+        view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
+    private let bottomBarStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
     // constraint Spacing
     private let userImageViewSize: CGFloat = 40
+    private let btnSize: CGFloat = 24
     
     //MARK: - setup
     private func setupViews() {
         let viewsToAdd: [UIView] = [
             userImageView,
+            notificationBtn,
+            tableView,
+            bottomBarView,
         ]
         viewsToAdd.forEach { view.addSubview($0) }
+        
+        // setup TabBar View
+        bottomBarView.addBottomBarStyle()
+        bottomBarView.addSubview(bottomBarStackView)
+        let home = createView(image: "HomeActive",
+                              title: "Home",
+                              tapAction: #selector(showHomeTab))
+        let account = createView(image: "AccountDefault",
+                                 title: "Account",
+                                 tapAction: #selector(showAccountTab))
+        let location = createView(image: "LocationActive",
+                                  title: "Location",
+                                  tapAction: #selector(showLocationTab))
+        let service = createView(image: "Service",
+                                 title: "Service",
+                                 tapAction:  #selector(showServiceTab))
+        let viewsToAddBottomBarStack: [UIView] = [
+            home,
+            account,
+            location,
+            service,
+        ]
+        viewsToAddBottomBarStack.forEach { bottomBarStackView.addArrangedSubview($0) }
     }
     
     private func setupConstraint() {
@@ -65,16 +104,108 @@ class ViewController: UIViewController {
         let bottomSafeArea = view.safeAreaLayoutGuide.bottomAnchor
         
         NSLayoutConstraint.activate([
-            // 由上到下、由左到右設計
+            userImageView.topAnchor.constraint(equalTo: topSafeArea, constant: 5),
+            userImageView.leftAnchor.constraint(equalTo: leftSafeArea, constant: 24),
+            userImageView.heightAnchor.constraint(equalToConstant: userImageViewSize),
+            userImageView.widthAnchor.constraint(equalToConstant: userImageViewSize),
+            
+            notificationBtn.rightAnchor.constraint(equalTo: rightSafeArea, constant: -24),
+            notificationBtn.centerYAnchor.constraint(equalTo: userImageView.centerYAnchor),
+            notificationBtn.heightAnchor.constraint(equalToConstant: btnSize),
+            notificationBtn.widthAnchor.constraint(equalToConstant: btnSize),
+            
+            tableView.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 5),
+            tableView.leftAnchor.constraint(equalTo: leftSafeArea),
+            tableView.rightAnchor.constraint(equalTo: rightSafeArea),
+            tableView.bottomAnchor.constraint(equalTo: bottomSafeArea),
+            
+            bottomBarView.leftAnchor.constraint(equalTo: leftSafeArea, constant: 24),
+            bottomBarView.rightAnchor.constraint(equalTo: rightSafeArea, constant: -24),
+            bottomBarView.bottomAnchor.constraint(equalTo: bottomSafeArea, constant: -22),
+            bottomBarView.heightAnchor.constraint(equalToConstant: 50),
+            
+            bottomBarStackView.topAnchor.constraint(equalTo: bottomBarView.topAnchor),
+            bottomBarStackView.leftAnchor.constraint(equalTo: bottomBarView.leftAnchor),
+            bottomBarStackView.rightAnchor.constraint(equalTo: bottomBarView.rightAnchor),
+            bottomBarStackView.bottomAnchor.constraint(equalTo: bottomBarView.bottomAnchor),
         ])
     }
     
+    /// Create a custom tab bar item view.
+    /// - Parameters:
+    ///   - image: The name of the image for the icon.
+    ///   - title: The title text for the item.
+    ///   - tapAction: The selector to be called when the item is tapped.
+    /// - Returns: A custom view representing a tab bar item.
+    private func createView(image: String, title: String, tapAction: Selector) -> UIView {
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.textColor = .black
+        titleLabel.font = .systemFont(ofSize: 12)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(imageView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: tapAction)
+        containerView.addGestureRecognizer(tapGesture)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5),
+            imageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 24),
+            imageView.widthAnchor.constraint(equalToConstant: 24),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5),
+            titleLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -5),
+        ])
+        return containerView
+    }
+    
+    @objc private func showHomeTab() {
+        print("showHome")
+    }
+    
+    @objc private func showAccountTab() {
+        print("showAccount")
+    }
+    
+    @objc private func showLocationTab() {
+        print("showLocation")
+    }
+    
+    @objc private func showServiceTab() {
+        print("showService")
+    }
+    
+    //MARK: View Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupConstraint()
+        
+        bottomBarView.addBottomBarStyle()
     }
 
-
 }
+
+//MARK: - TableView
+//extension ViewController: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//}
 
