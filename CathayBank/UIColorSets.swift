c242e017c1b85234e78b2bd18572092f7abb4adf
EricYou <zl2408399@gmail.com>
2023-08-18 18:39:20 +0800

    feat: add color extension

--- CathayBank/UIColorSets.swift
+++ CathayBank/UIColorSets.swift
@@ -7,13 +7,13 @@ import UIKit
 extension UIColor {
     //MARK: - 灰
     /// 淡灰
-    static let lightGray: UIColor = { return UIColor.init(red: 136 / 255.0, green: 136 / 255.0, blue: 136 / 255.0, alpha: 1.0) }()
+    static let lightGray: UIColor = { return UIColor.init(red: 136, green: 136, blue: 136, alpha: 1) }()
 
     /// 木炭灰
-    static let charcoalGray: UIColor = { return UIColor.init(red: 85 / 255.0, green: 85 / 255.0, blue: 85 / 255.0, alpha: 1.0) }()
+    static let charcoalGray: UIColor = { return UIColor.init(red: 85, green: 85, blue: 85, alpha: 1.0) }()
 
     /// 暗灰色
-    static let dimGray: UIColor = { return UIColor.init(red: 68 / 255.0, green: 68 / 255.0, blue: 68 / 255.0, alpha: 1.0) }()
+    static let dimGray: UIColor = { return UIColor.init(red: 68, green: 68, blue: 68, alpha: 1.0) }()
 
 }
 
@@ -34,5 +34,12 @@ extension UIColor {
             (a, r, g, b) = (255, 0, 0, 0)
         }
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        let redValue = CGFloat(red) / 255.0
+        let greenValue = CGFloat(green) / 255.0
+        let blueValue = CGFloat(blue) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue, alpha: alpha)
     }
 }
