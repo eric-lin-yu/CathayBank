21cfa7333f29e0b36e94e562cccd439d78552c59
EricLin <zl2408399@gmail.com>
2023-09-01 15:31:57 +0800

    feat: API Info 整理

--- CathayBank/APIManager/APIInfo.swift
+++ CathayBank/APIManager/APIInfo.swift
@@ -10,34 +10,34 @@ import Foundation
 struct APIInfo {
     // API domain
     private static let baseDomain = "https://willywu0201.github.io/data/"
-    
-    // Favorites
-    public static let favoriteList = baseDomain + "favoriteList.json"
-    
+        
     // Notifications
     public static let notificationList = baseDomain + "notificationList.json"
     public static let emptyNotificationList = baseDomain + "emptyNotificationList.json"
 
-    // 初次登入
     // USD
-    public static let firstUSD = baseDomain + "usdSavings1.json" // Savings
-    public static let firstUSDFixedDeposited = baseDomain + "usdFixed1.json" // Fixed Deposits
-    public static let firstUSDDigital = baseDomain + "usdDigital1.json" // Digital Accounts
+    public static let firstUSD = baseDomain + "usdSavings1.json"
+    public static let firstUSDFixedDeposited = baseDomain + "usdFixed1.json"
+    public static let firstUSDDigital = baseDomain + "usdDigital1.json"
+    
+    public static let refreshUSD = baseDomain + "usdSavings2.json"
+    public static let refreshUSDFixedDeposited = baseDomain + "usdFixed2.json"
+    public static let refreshUSDDigital = baseDomain + "usdDigital2.json"
     
     // KHR
-    public static let firstKHR = baseDomain + "khrSavings1.json" // Savings
-    public static let firstKHRFixedDeposited = baseDomain + "khrFixed1.json" // Fixed Deposits
-    public static let firstKHRDigital = baseDomain + "khrDigital1.json" // Digital Accounts
+    public static let firstKHR = baseDomain + "khrSavings1.json"
+    public static let firstKHRFixedDeposited = baseDomain + "khrFixed1.json"
+    public static let firstKHRDigital = baseDomain + "khrDigital1.json"
     
-    // 重整抓取資料
-    // USD
-    public static let refreshUSD = baseDomain + "usdSavings2.json" // Savings
-    public static let refreshUSDFixedDeposited = baseDomain + "usdFixed2.json" // Fixed Deposits
-    public static let refreshUSDDigital = baseDomain + "usdDigital2.json" // Digital Accounts
+    public static let refreshKHR = baseDomain + "khrSavings2.json"
+    public static let refreshKHRFixedDeposited = baseDomain + "khrFixed2.json"
+    public static let refreshKHRDigital = baseDomain + "khrDigital2.json"
     
-    // KHR
-    public static let refreshKHR = baseDomain + "khrSavings2.json" // Savings
-    public static let refreshKHRFixedDeposited = baseDomain + "khrFixed2.json" // Fixed Deposits
-    public static let refreshKHRDigital = baseDomain + "khrDigital2.json" // Digital Accounts
+    // Favorites
+    public static let favoriteList = baseDomain + "favoriteList.json"
+    public static let emptyFavoriteList = baseDomain + "emptyFavoriteList.json"
+    
+    // ad
+    public static let adBanner = baseDomain + "banner.json"
 }
 
