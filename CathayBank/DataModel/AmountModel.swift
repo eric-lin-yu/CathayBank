8ece822364e41d651ac174fec14c6d6525202b32
EricLin <zl2408399@gmail.com>
2023-09-01 12:21:54 +0800

    feat: AmountModel 架構調整

--- CathayBank/DataModel/AmountModel.swift
+++ CathayBank/DataModel/AmountModel.swift
@@ -9,19 +9,21 @@ import Foundation
 struct AmountResponse: Codable {
     let msgCode: String
     let msgContent: String
-    let result: AmountResult
+    let result: [String: [AmountModel]]
+    
+    init(msgCode: String, msgContent: String, result: [String: [AmountModel]]) {
+        self.msgCode = msgCode
+        self.msgContent = msgContent
+        self.result = result
+    }
 }
 
-struct AmountResult: Codable {
-    let savingsList: [AmountAccount]
-}
-
-struct AmountAccount: Codable {
+struct AmountModel: Codable {
     let account: String
     let curr: String
-    let balance: Double
+    let balance: Float
     
-    init(account: String, curr: String, balance: Double) {
+    init(account: String, curr: String, balance: Float) {
         self.account = account
         self.curr = curr
         self.balance = balance
