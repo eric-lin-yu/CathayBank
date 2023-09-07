13c03281c27ee64a5f0b9f05c56bf29854b32041
EricLin <zl2408399@gmail.com>
2023-09-07 11:11:01 +0800

    feat: add Navigation

---
+++ CathayBank/CathayNavigationController.swift
@@ -0,0 +1,42 @@
+//
+//
+//  Created by Eric Lin on 2023/1/11.
+//  Copyright © 2019 Eric Lin. All rights reserved.
+//
+
+import UIKit
+
+class CathayNavigationController: UINavigationController {
+    override var navigationBar: UINavigationBar {
+        let bar = super.navigationBar
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .white
+        appearance.backButtonAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: -1000, vertical: 0)
+        bar.tintColor = .black
+        bar.standardAppearance = appearance
+        bar.scrollEdgeAppearance = appearance
+        return bar
+    }
+    
+    /**
+     implementation of init(rootViewController:) will call self.init(nibName:bundle:)
+     so we have to override init(nibName:bundle:) to prevent crash
+     */
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+  
+}
--- CathayBank/ViewController.swift
+++ CathayBank/ViewController.swift
@@ -81,6 +81,7 @@ class ViewController: UIViewController {
 
     //MARK: - setup UI
     private func setupViews() {
+        view.backgroundColor = .white
         let viewsToAdd: [UIView] = [
             userImageView,
             notificationBtn,
@@ -150,6 +151,16 @@ class ViewController: UIViewController {
         
         tableView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     @objc private func refreshData(_ sender: Any) {
